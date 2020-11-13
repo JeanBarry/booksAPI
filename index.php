8e96a0a99273a27000dca48f89daa4e0683dcdf4
Jean Barry <jfvbarry@gmail.com>
2020-11-13 12:02:30 -0600

    add basic home design

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BooksAPI</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <ul class="nav-list">
+                <li class="nav-item"><a href="#">Home</a></li>
+                <li class="nav-item"><a href="#">Books</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="main-section">
+            <h1 class="main-title">BooksAPI</h1>
+            <p class="main-subtitle">Simple API for books</p>
+        </section>
+    </main>
+    
+</body>
+</html>
